5da03c694c4b0f79b510badabf2f9b86b56a0c0e
sam1 <sam1@abc.com>
2017-02-19 23:48:05 +0000

    Updated by testb branch

--- helloworld/src/test/java/helloworld/SampleTest.java
+++ helloworld/src/test/java/helloworld/SampleTest.java
@@ -15,7 +15,19 @@ public class SampleTest {
 		assertTrue(a + b == 3);
 		
 	}
+	## added bt devbranch
 	
+	@Test
+	public void test() {
+		
+		int a = 1;
+		int b = 2;
+		
+		assertTrue(a + b == 4);
+		
+	}
+	
+	# added by testb
 	@Test
 	public void test() {
 		
